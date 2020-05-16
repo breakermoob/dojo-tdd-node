f4c2bac24799d592195d393a447c18caaf6234e5
Breakermoob <breakermoob@hotmail.com>
2020-05-16 09:29:16 -0500

    calculator test

---
+++ calculator.test.js
@@ -0,0 +1,24 @@
+const calculator = require('./calculator');
+
+describe('adds operations', () => {
+   test('adds 1 + 2 to equal 3', () => {
+      expect(calculator.add(1, 2)).toBe(3);
+   });
+   test('adds 5 + 5 to equal 10', () => {
+      expect(calculator.add(5, 5)).toBe(10);
+   });
+})
+
+
+
+describe('subtracts operations', () => {
+   test('subtract 5 - 5 to equal 0', () => {
+      expect(calculator.subtract(5, 5)).toBe(0);
+   });
+   test('subtract 5 - 1 to equal 4', () => {
+      expect(calculator.subtract(5, 1)).toBe(4);
+   });
+   test('subtract 5 - 1 to equal 4', () => {
+      expect(calculator.subtract(5, -1)).toBe(6);
+   });
+})
