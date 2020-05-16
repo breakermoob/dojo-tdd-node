ee11fb9b915b14aa980f5cf008b6ee6cac9aecae
Breakermoob <breakermoob@hotmail.com>
2020-05-16 12:33:44 -0500

    CodeBreaker Full Test

---
+++ codebreaker.js
@@ -0,0 +1,45 @@
+'use sctrict'
+
+class CodeBreaker {
+   constructor(newSecret) {
+      this.secret = newSecret
+   }
+
+   setSecret(value) {
+      this.secret = value;
+   }
+
+   guest(value) {
+      let response = '';
+      for (let i = 0; i < value.length; i++) {
+         if (this.itExist(value[i])) {
+            if (this.isPositioned(value[i], i)) {
+               response = 'X' + response;
+            } else {
+               response = response + '_';
+            }
+         } else {
+            // response = response + ' ';
+         }
+      }
+      return response;
+   }
+
+   itExist(digit) {
+      if (this.secret.includes(digit)) {
+         return true
+      } else {
+         return false
+      }
+   }
+
+   isPositioned(value, position) {
+      if (this.secret[position] == value) {
+         return true
+      } else {
+         return false
+      }
+   }
+}
+
+module.exports = CodeBreaker;
