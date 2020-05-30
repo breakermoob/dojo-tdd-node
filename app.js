a970dfca5564aee76562d8f5bb37e18437a98518
Breakermoob <breakermoob@hotmail.com>
2020-05-30 12:11:39 -0500

    cors

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const app = express();
 const cal = require('./src/calculator');
 const CodeBreaker = require('./src/codebreaker');
 const cb = new CodeBreaker('0123');
+const cors = require('cors')
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 app.get('/calculator/add', async (req, res) => {
    try {
